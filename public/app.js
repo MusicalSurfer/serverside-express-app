d62ce7eb71d1ac329f5618c7b26dc9698fc02f62
DANIEL_GRADY <anakria977@gmail.com>
2024-01-25 13:39:56 -0500

    Add front side JS to start to display data

---
+++ public/app.js
@@ -0,0 +1,31 @@
+const webObj = {
+    mainProgram: () => {
+        const caliberLinkButton = document.getElementById('caliberLink');
+        caliberLinkButton.addEventListener('click', webObj.caliberLinkHandler);
+    },
+    caliberLinkHandler: () => {
+        fetch('https://eft-ballistics-deploy.onrender.com/eft/ballistics') // Returns a promise that either resolves or doesn't.
+            .then(response => {
+                // Check if the response status is 'ok' (200)
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                // Parse the JSON response
+                console.log('promise resolved');
+                return response.json();
+            })
+            .then(data => {
+                // Display the data on the page.
+                console.log(data);
+                const resultDiv = document.getElementById('results');
+                const resultTitle = document.createElement('div');
+                resultTitle.id = 'resultTitle';
+                resultDiv.appendChild(resultTitle);
+            })
+            .catch(error => {
+                // Display an error message if the request fails\
+                console.error(error);
+            });
+    }
+}
+webObj.mainProgram();
