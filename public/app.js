5d1b5a9944ce15500c9464a31ed7609a41ec2db4
DANIEL_GRADY <anakria977@gmail.com>
2024-01-25 14:19:59 -0500

    Troubleshoot data presentation.

--- public/app.js
+++ public/app.js
@@ -14,9 +14,8 @@ const webObj = {
             })
             .then(data => {
                 console.log(data);
-                for (let caliber in data) {
+                for (let caliber of data.data) {
                     console.log(caliber.name);
-                    console.log(data.name);
                     const resultDiv = document.getElementById('results');
                     const resultTitle = document.createElement('div');
                     resultTitle.id = 'resultTitle';
